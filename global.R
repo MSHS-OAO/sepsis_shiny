bf5a2e23e81c5cbe5ad95c59d30c16f593398532
armando-v <>
2024-04-11 14:44:17 -0400

    update ui and server

--- global.R
+++ global.R
@@ -9,6 +9,7 @@ library(shinycssloaders)
 library(rhandsontable)
 library(glue)
 library(assertr)
+library(shinyWidgets)
 
 
 # Read in Data ------------------------------------------------------------
@@ -25,10 +26,9 @@ data <- data_raw %>%
 
 
 # Filter Defaults ---------------------------------------------------------
-shown_time_est_default <- data %>% select(alert_initially_shown_time_est) %>% distinct() %>% pull(alert_initially_shown_time_est)
 dept_type_census_default <- data %>% select(department_type_census) %>% distinct() %>% pull(department_type_census) 
 alert_subtype_default <- data %>% select(alert_subtype) %>% distinct() %>% pull(alert_subtype) 
 outcome_category_default <- data %>% select(outcome_category) %>% distinct() %>% pull(outcome_category)
-
-
+min_est_date <- data %>% mutate(min_date = min(alert_initially_shown_time_est)) %>% select(min_date) %>% distinct() %>% mutate(min_date = format(min_date, "%Y-%m-%d")) %>% pull(min_date)
+max_est_date <- data %>% mutate(max_date = max(alert_initially_shown_time_est)) %>% select(max_date) %>% distinct() %>% mutate(max_date = format(max_date, "%Y-%m-%d")) %>% pull(max_date)
 
---
+++ ui.R
@@ -0,0 +1,39 @@
+ui <- fluidPage(
+        titlePanel(
+          h1("Clinical Command Center - Sepsis Use Case Process Metrics", style = "color:black", align = 'center'),
+        ),
+        fluidRow(
+          column(3,
+                 dateRangeInput(
+                   inputId = "time_est",
+                   label = NULL,
+                   start = min_est_date,
+                   end = max_est_date
+                 )
+                 ),
+          column(3,
+                 pickerInput(inputId = "dept_type_census",
+                             label = NULL,
+                             multiple = TRUE,
+                             choices = dept_type_census_default,
+                             selected = dept_type_census_default
+                 )
+                 ),
+          column(3,
+                 pickerInput(inputId = "alert_subtype",
+                             label = NULL,
+                             multiple = TRUE,
+                             choices = alert_subtype_default,
+                             selected = alert_subtype_default)
+                 ),
+          column(3,
+                 pickerInput(inputId = "outcome_category", 
+                             label = NULL,
+                             multiple = TRUE,
+                             choices = alert_subtype_default,
+                             selected = alert_subtype_default)
+                 )
+
+        )
+      
+    )
