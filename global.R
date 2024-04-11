70851d92c55e6640083366eb287a2ba9ef05237b
armando-v <>
2024-04-11 13:57:55 -0400

    update raw data filters

--- global.R
+++ global.R
@@ -18,11 +18,17 @@ data_raw <- read_csv(paste0(base_file_path, "care_alerts_external_reporting_copy
 
 # Raw Data Processing --------------------------------------------------------
 data <- data_raw %>% 
-  filter(alert_after_pilot_start_date == TRUE, improperly_flagged != TRUE, in_msb_pilot == TRUE, alert_initially_shown_time >= as.POSIXct("2024-01-11 00:00:00"))
+  filter(alert_after_pilot_start_date == TRUE & (improperly_flagged == FALSE | is.na(improperly_flagged)) & in_msb_pilot == TRUE, alert_initially_shown_time >= as.POSIXct("2024-01-11 00:00:00")
+         )
 
 
 
 
 # Filter Defaults ---------------------------------------------------------
+shown_time_est_default <- data %>% select(alert_initially_shown_time_est) %>% distinct() %>% pull(alert_initially_shown_time_est)
+dept_type_census_default <- data %>% select(department_type_census) %>% distinct() %>% pull(department_type_census) 
+alert_subtype_default <- data %>% select(alert_subtype) %>% distinct() %>% pull(alert_subtype) 
+outcome_category_default <- data %>% select(outcome_category) %>% distinct() %>% pull(outcome_category)
+
 
 
