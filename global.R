be98f1ef851875b9b3f3d3e65f49ec04f9864155
armando-v <>
2024-04-11 13:20:41 -0400

    read in data

--- global.R
+++ global.R
@@ -9,3 +9,8 @@ library(shinycssloaders)
 library(rhandsontable)
 library(glue)
 library(assertr)
+
+
+base_file_path <- "/SharedDrive/deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/System Operations/Sepsis Module Dashboard/"
+data <- read_csv(paste0(base_file_path, "care_alerts_external_reporting_copy.csv"))
+
