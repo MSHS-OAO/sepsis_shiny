d9514aac250940f89da78798aedf8b17963e3a59
armando-v <>
2024-04-18 13:03:33 -0400

    add table for volume by alert type

--- global.R
+++ global.R
@@ -11,6 +11,7 @@ library(glue)
 library(assertr)
 library(shinyWidgets)
 library(janitor)
+library(kableExtra)
 
 
 # Read in Data ------------------------------------------------------------
@@ -32,4 +33,37 @@ alert_subtype_default <- data %>% select(alert_subtype) %>% distinct() %>% pull(
 outcome_category_default <- data %>% select(outcome_category) %>% distinct() %>% pull(outcome_category)
 min_est_date <- data %>% mutate(min_date = min(alert_initially_shown_time_est)) %>% select(min_date) %>% distinct() %>% mutate(min_date = format(min_date, "%Y-%m-%d")) %>% pull(min_date)
 max_est_date <- data %>% mutate(max_date = max(alert_initially_shown_time_est)) %>% select(max_date) %>% distinct() %>% mutate(max_date = format(max_date, "%Y-%m-%d")) %>% pull(max_date)
+
+
+
+
+###Functions to calculate volume by alert type
+volume_by_alert_type <- function(data, data_total, column_name) {
+  ### Total ALerts Calulcations
+  
+  count_column_name <- paste0(column_name, " Count")
+  total_column_name <- paste0(column_name, " Total")
+  percent_column_name <- paste0(column_name, " Percent")
+  
+  total_alerts <- data %>% group_by(alert_subtype) %>% summarise("{count_column_name}" := n()) %>%
+    mutate("{total_column_name}" := sum(.data[[count_column_name]])) %>% mutate("{percent_column_name}" := formattable::percent(.data[[count_column_name]]/.data[[total_column_name]])) %>%
+    select(-!!sym(total_column_name))
+  
+  total_alerts_grand_total <-  data_total %>% summarise("{total_column_name}" := n()) %>% 
+    mutate(alert_subtype = "Grand Total")
+  
+  total_alerts_sum <- total_alerts %>% summarise("{count_column_name}" := sum(.data[[count_column_name]])) %>%
+                      mutate(alert_subtype = "Grand Total")
+  
+  combine_grandtotal <- full_join(total_alerts_sum, total_alerts_grand_total) %>%
+                        mutate("{percent_column_name}" := formattable::percent(.data[[count_column_name]]/.data[[total_column_name]])) %>%
+                        select(-!!sym(total_column_name))
+  
+  total_alerts_combined <- bind_rows(total_alerts, combine_grandtotal)
+  
+  # total_alerts_combined <- column_to_rownames(total_alerts_combined, "alert_subtype")
+  
+  return(total_alerts_combined)
+  
+}
 
--- server.R
+++ server.R
@@ -7,9 +7,20 @@ server <- function(input, output, session) {
   output$volume_by_alert_type <- function() {
     data <- data %>% filter(alert_initially_shown_time_est >= as.POSIXct("2024-03-15 00:00:00"))
     
-    table_data <- data %>% group_by(alert_subtype) %>% summarise(Count = n()) %>%
-                  adorn_totals("row")
     
-    kable(data, )
+    total_alerts_data <- volume_by_alert_type(data, data, "total")
+    total_alerts_ch_action <- volume_by_alert_type(data %>% filter(alert_has_ch_action == TRUE), data, "ch action")
+    alerts_epic_resolution <- volume_by_alert_type(data %>% filter(alert_resolved_flag == TRUE), data, "epic resolution")
+    
+    
+    data_combined <- total_alerts_data %>% full_join(total_alerts_ch_action) %>% full_join(alerts_epic_resolution)
+    
+    column_names <- c("Alert Subtype", "Count", "%", "Alerts with CH Action", "% w CH Action", "Count Resolved", "% Resolved")
+    
+    kable(data_combined, "html", align = "c", col.names = column_names) %>%
+      add_header_above(c("  " = 1, "Total Alerts" = 2, "Alerts with CH Action" = 2, "Alerts with Epic Resolultion" = 2),background = "#212070", color = "white")%>%
+      kable_styling(bootstrap_options = c("hover", "bordered", "striped"), 
+                    full_width = FALSE, position = "center", 
+                    row_label_position = "c", font_size = 16)
   }
 }
